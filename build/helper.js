421ff075c71aa4a7a55188114193653d40b3a8b1
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2022-01-06 23:52:57 +0100

    Update helper.js

--- build/helper.js
+++ build/helper.js
@@ -5,15 +5,19 @@
 const fse = require('fs-extra');
 const util = require("util");
 const rimRaf = util.promisify(require("rimraf"));
-const chalk = require('chalk');
+const pc = require('picocolors');
 const crypto = require('crypto');
+
+// Activate for "module.exports.unminifyCss"!
+// const recursive = require("recursive-readdir");
+// const unminifyCss = require('cssunminifier').unminify;
 
 module.exports.cleanOut = async (cleanOuts) =>
 {
 	for (const file of cleanOuts)
 	{
 		await rimRaf(file).then(
-			answer => console.log(chalk.redBright(`rimRafed "${file}".`))
+			answer => console.log(pc.red(pc.bold(`rimRafed "${file}".`)))
 		).catch(error => console.error('Error ' + error));
 	}
 }
@@ -47,8 +51,8 @@ module.exports.getChecksum = async (path, Digest) =>
 // Find version string in file. E.g. 'scssphp/scssphp'
 module.exports.findVersionSub = async (packagesFile, packageName) =>
 {
-	console.log(chalk.magentaBright(
-	`Search versionSub of package "${packageName}" in "${packagesFile}".`));
+	console.log(pc.magenta(pc.bold(
+	`Search versionSub of package "${packageName}" in "${packagesFile}".`)));
 
 	let foundVersion = '';
 	const {packages} = require(packagesFile);
@@ -63,4 +67,44 @@ module.exports.findVersionSub = async (packagesFile, packageName) =>
 	});
 
 	return foundVersion;
+}
+
+// Simple. Find version string in file package.json
+module.exports.findVersionSubSimple = async (packagesFile, packageName) =>
+{
+	console.log(pc.magenta(pc.bold(
+	`Search versionSub of package "${packageName}" in "${packagesFile}".`)));
+
+	return require(packagesFile).version;
+}
+
+// Unminify recursive. All *.min.css to *.css
+// Usage in build.js: await helper.unminifyCss(folderPath);
+module.exports.unminifyCss = async (folder) =>
+{
+	await recursive(folder).then(
+		function(files) {
+			const thisRegex = new RegExp('\.min\.css$');
+
+			files.forEach((file) => {
+				file = `./${file}`;
+
+				if (thisRegex.test(file) && fse.existsSync(file)
+					&& fse.lstatSync(file).isFile())
+				{
+					console.log(pc.magenta(pc.bold(`File to unminify: ${file}`)));
+					let unminifiedFile = file.replace('.min.css', '.css');
+					let code = fse.readFileSync(`${file}`).toString();
+					code = unminifyCss(code);
+					fse.writeFileSync(unminifiedFile, code, {encoding: "utf8"});
+					console.log(pc.green(pc.bold(
+						`Unminified file written: ${unminifiedFile}`))
+					);
+				}
+			});
+		},
+		function(error) {
+			console.error("something exploded", error);
+		}
+	);
 }
